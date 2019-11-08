79885b986d96b06ff37bc212fc0a4b16e9081391
mithun <mithunhrm07@gamil.com>
2019-11-08 17:15:43 +0530

    completed 70%

--- javascript/file1.js
+++ javascript/file1.js
@@ -13,7 +13,42 @@ function carousel() {
   setTimeout(carousel, 2000); 
 
 
+<section id="page1">
+			<div class="container">
+	  		<img src="images/image1.jpg" alt="Snow" style="width:70%;">
+	 	 		<div class="centered">We are the best <br> consulting agency <br></div>
+	 	 		<div class="centered1">Ultra Consultants is an independent research advisory and <br> consulting firm serving the manufacturing and<br> distribution industries.</div>
+	 	 		<a href="#page5"><div class="centered2"><button type="button" style="background-color: #d4125a">Get started</button></div></a>
+			</div>
+		</section>
 
 
-  
-  
+
+
+
+		.container {
+		  position: relative;
+		  text-align: center;
+		  color: white;
+		}
+		.centered {
+		  position: absolute;
+		  top: 30%;
+		  left: 50%;
+		  transform: translate(-50%, -50%);
+		  color: black;
+		  font-size: 30px;
+		}
+		.centered1{
+		  position: absolute;
+		  top: 48%;
+		  left: 50%;
+		  transform: translate(-50%, -50%);
+		  color: black;
+		}
+		.centered2{
+		  position: absolute;
+		  top: 60%;
+		  left: 50%;
+		  transform: translate(-50%, -50%);
+		}
